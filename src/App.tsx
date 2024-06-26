8f3a9c7a362a0d7319bec7c1c00cf73f50cf5653
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-26 12:47:54 +0700

    Created open state to holding modal state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Header from 'components/Header'
+import FormData from 'components/FormData'
+import { useState } from 'react'
 
 function App() {
+  const [open, setOpen] = useState<boolean>(false)
   return (
     <div className='App'>
-      <Header></Header>
+      <Header setOpen={setOpen} />
+      <div className='container mx-auto max-w-7xl px-6 lg:px-8'>
+        <div className='flex justify-end'></div>
+      </div>
+
+      <FormData open={open} setOpen={setOpen} />
     </div>
   )
 }
