d7afeee55ce3e1f69060cee887acae163531739a
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-26 12:45:26 +0700

    Config custom minWidth

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      minWidth: {
+        custom: '600px'
+      }
+    }
   },
   plugins: []
 }
