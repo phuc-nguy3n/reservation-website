882860a8363cd29d82cd5334d260357b0b81ceb5
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-27 13:40:21 +0700

    Remove open state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import Header from 'components/Header'
-import FormData from 'components/FormData'
-import { useState } from 'react'
 import Container from 'components/Container'
 import Blogs from 'components/Blogs'
 
 function App() {
-  const [open, setOpen] = useState<boolean>(false)
   return (
     <div className='App'>
-      <Header setOpen={setOpen} />
+      <Header />
       <Container>
         <Blogs />
       </Container>
-
-      <FormData open={open} setOpen={setOpen} />
     </div>
   )
 }
