88eea85129b42f23af435a91708b62773789f3b7
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-27 14:15:49 +0700

    Import Outlet to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import Header from 'components/Header'
-import Container from 'components/Container'
-import Blogs from 'components/Blogs'
+import BlogPage from 'pages/BlogPage/BlogPage'
+import { Outlet } from 'react-router'
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Container>
-        <Blogs />
-      </Container>
+      <Outlet />
     </div>
   )
 }
