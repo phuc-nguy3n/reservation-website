e6e3cdb63e2dee72dfc16729516b7c51a2e42b06
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-28 15:22:52 +0700

    Use Link from react-router-dom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel, PopoverGroup } from '@headlessui/react'
 import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
 
 type NavItem = {
   name: string
@@ -12,9 +13,9 @@ type Nav = NavItem[]
 
 const Header = () => {
   const navList: Nav = [
-    { name: 'Home', path: '#', current: false },
-    { name: 'About', path: '#about', current: false },
-    { name: 'Blog', path: '#blog', current: true }
+    { name: 'Home', path: '/', current: true },
+    { name: 'About', path: '/about', current: false },
+    { name: 'Blog', path: '/blog', current: false }
   ]
 
   const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false)
@@ -37,14 +38,14 @@ const Header = () => {
       <header className='bg-white'>
         <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8' aria-label='Global'>
           <div className='flex lg:flex-1'>
-            <a href='/' className='-m-1.5 p-1.5'>
+            <Link to='/' className='-m-1.5 p-1.5'>
               <span className='sr-only'>Your Company</span>
               <img
                 className='h-8 w-auto'
                 src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
                 alt=''
               />
-            </a>
+            </Link>
           </div>
 
           <div className='flex lg:hidden'>
@@ -60,14 +61,14 @@ const Header = () => {
 
           <PopoverGroup className='hidden items-center lg:flex lg:gap-x-12'>
             {navItems.map((item) => (
-              <a
+              <Link
                 key={'nav-' + item.name}
-                href={item.path}
+                to={item.path}
                 className={`${item.current ? 'text-gray-500' : 'text-gray-900'} text-sm font-semibold leading-6 hover:text-gray-500`}
                 onClick={() => activeNavItem(item)}
               >
                 {item.name}
-              </a>
+              </Link>
             ))}
           </PopoverGroup>
         </nav>
@@ -89,13 +90,14 @@ const Header = () => {
               <div className='-my-6 divide-y divide-gray-500/10'>
                 <div className='space-y-2 py-6'>
                   {navItems.map((item) => (
-                    <a
+                    <Link
                       key={'nav-' + item.name}
-                      href={item.path}
-                      className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50'
+                      to={item.path}
+                      className={`${item.current ? 'bg-gray-50' : ''} -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50`}
+                      onClick={() => activeNavItem(item)}
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </div>
