6823784d534d8f97a7ef611cbcc6235b73d7c881
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-29 22:29:20 +0700

    Fix active nav when user click logo then open another tab

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,7 +61,14 @@ const Header = () => {
       <header className='bg-white'>
         <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8' aria-label='Global'>
           <div className='flex lg:flex-1'>
-            <Link to='/' className='-m-1.5 p-1.5'>
+            <Link
+              onClick={() => {
+                setNavItems(navList)
+                localStorage.setItem('navItems', JSON.stringify(navList))
+              }}
+              to='/'
+              className='-m-1.5 p-1.5'
+            >
               <span className='sr-only'>Your Company</span>
               <img
                 className='h-8 w-auto'
