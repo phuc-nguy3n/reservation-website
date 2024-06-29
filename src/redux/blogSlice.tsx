34d855df97c7bb18d40dd17296a1e482968bacae
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-29 23:58:25 +0700

    Add id to initialState

--- src/redux/blogSlice.tsx
+++ src/redux/blogSlice.tsx
@@ -3,6 +3,7 @@ import { BlogItems, BlogType } from '../components/Blogs'
 
 const initialState: BlogItems = [
   {
+    id: '1',
     title: 'How to quickly deploy a static website',
     img: 'https://images.unsplash.com/photo-1531804055935-76f44d7c3621?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     description:
