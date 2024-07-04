a8bb1d8bb85f9b050dcc29aaec375700bd334c28
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-07-04 23:06:12 +0700

    Img loader display when imageLoaded[index]  is undefined

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -9,19 +9,40 @@ type BlogType = {
 type BlogItemProps = {
   blog: BlogType
   index: number
+  imageLoaded: boolean[]
+  setImageLoaded: React.Dispatch<React.SetStateAction<boolean[]>>
 }
 
-const BlogItem: React.FC<BlogItemProps> = ({ blog, index }) => {
+const BlogItem: React.FC<BlogItemProps> = ({ blog, index, imageLoaded, setImageLoaded }) => {
   return (
     <article
       key={'blog-item-' + index}
       className='flex h-blogItem flex-row gap-x-4 rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800'
     >
       <div className='box-img w-2/5'>
-        <img className='h-full w-full rounded object-cover' src={blog.img} alt='' />
+        {imageLoaded[index] === undefined && (
+          <img
+            className={`h-full w-full rounded object-cover ${imageLoaded[index] === undefined ? 'block' : 'hidden'}`}
+            src={require('../resources/img/image_loading.gif')}
+            alt={`img-blog-${index}`}
+          />
+        )}
+        <img
+          className={`h-full w-full rounded object-cover ${imageLoaded[index] ? 'block' : 'hidden'}`}
+          src={blog.img}
+          alt={`img-blog-${index}`}
+          onLoad={() => {
+            setImageLoaded((prevImageLoaded) => {
+              const newArr = [...prevImageLoaded]
+              newArr[index] = true
+              console.log(newArr)
+              return newArr
+            })
+          }}
+        />
       </div>
       <div className='w-3/5'>
-        <h2 className='max-h-blogTitle mb-2 overflow-hidden text-ellipsis text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+        <h2 className='mb-2 max-h-blogTitle overflow-hidden text-ellipsis text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
           {blog.title}
         </h2>
         <p className='max-w-blogItem h-blogShortContent overflow-hidden text-ellipsis font-light text-gray-500 dark:text-gray-400'>
