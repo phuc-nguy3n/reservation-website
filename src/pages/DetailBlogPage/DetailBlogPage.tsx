b71ea884e6c3aea0b4611cc27529f433816c2cc1
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-07-05 21:02:31 +0700

    Created DetailBlogPage

---
+++ src/pages/DetailBlogPage/DetailBlogPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../../components/Container'
+import DetailBlog from 'components/DetailBlog'
+
+type Props = {}
+
+const DetailBlogPage = (props: Props) => {
+  return (
+    <Container>
+      <DetailBlog />
+    </Container>
+  )
+}
+
+export default DetailBlogPage
